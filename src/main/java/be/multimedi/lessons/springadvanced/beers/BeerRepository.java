66e20ae904223de202dd8aceb3a59ee5d0b25b05
Nick Bauters <nick.bauters@multimedi.be>
2019-10-29 22:19:34 +0100

    Opdracht 5 afgewerkt. Bug uit Opdr4 nnog steeds aanwezig

--- src/main/java/be/multimedi/lessons/springadvanced/beers/BeerRepository.java
+++ src/main/java/be/multimedi/lessons/springadvanced/beers/BeerRepository.java
@@ -1,14 +1,44 @@
 package be.multimedi.lessons.springadvanced.beers;
 
 import be.multimedi.lessons.springadvanced.beers.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
+import java.util.stream.Stream;
 
-public interface BeerRepository {
-    Beer getBeerById(int id);
+@Transactional
+public interface BeerRepository extends JpaRepository<Beer, Integer> {
+
+    default Beer getBeerById(int id) {
+        return findById(id).orElse(null);
+    }
+
+    @Query(name = "findByAlcohol")
     List<Beer> getBeerByAlcohol(double alcohol);
-    void updateBeer(Beer b);
+
+    @Transactional
+    default void updateBeer(Beer b) {
+        save(b);
+    }
+
+//    public List<Beer> getBeerByAlcohol(float alcohol);
+    public List<Beer> findByAlcoholOrderByNameAsc(float alcohol);
+    public List<Beer> findBeerByAlcoholBetween(float min, float max);
+    public List<Beer> findBeerByAlcoholGreaterThan(float min);
+    public List<Beer> findBeerByAlcoholLessThan(float max);
+    public List<Beer> findByAlcoholOrName(float alcohol, String name);
+    public List<Beer> findByNameLikeOrderByName(String name);
+    public List<Beer> findFirst10ByAlcohol(float alcohol);
+    public Stream<Beer> findByStockLessThan(int stock);
+
+    @Transactional
+    @Modifying
+    @Query("update Beer b set b.price=b.price*?1")
+    public int updatePrice(float perc);
 }
--- src/main/java/be/multimedi/lessons/springadvanced/orders/BeerOrderRepository.java
+++ src/main/java/be/multimedi/lessons/springadvanced/orders/BeerOrderRepository.java
@@ -1,6 +1,16 @@
 package be.multimedi.lessons.springadvanced.orders;
 
-public interface BeerOrderRepository {
-    int saveOrder(BeerOrder order);
-    BeerOrder getBeerOrderById(int id);
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public interface BeerOrderRepository extends JpaRepository<BeerOrder, Integer> {
+    default int saveOrder(BeerOrder order) {
+        save(order);
+        return order.getId();
+    }
+    default BeerOrder getBeerOrderById(int id) {
+        return findById(id).orElse(null);
+    }
 }
